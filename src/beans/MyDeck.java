8bd4046b8ab8697dcd463919f79a9a6ced8b2f67
Unknown <ale_antonioli@hotmail.com>
2018-05-17 10:43:10 +0100

    MyDeck class

--- src/beans/MyDeck.java
+++ src/beans/MyDeck.java
@@ -1,4 +1,29 @@
 package beans;
 
+import java.util.ArrayList;
+
 public class MyDeck {
+
+	private ArrayList<MyCard> deck = new ArrayList<MyCard>();
+
+	public MyDeck() {
+		String 
+		MyCard card = new MyCard();
+		for (int i = 1; i <= 13; i++) {
+			card.deck.add(card);
+		}
+	}
+
+	public ArrayList<MyCard> getDeck() {
+		return deck;
+	}
+
+	public void setDeck(ArrayList<MyCard> deck) {
+		this.deck = deck;
+	}
+
+	public int numberOfCards() {
+		return this.deck.size();
+	}
+
 }
