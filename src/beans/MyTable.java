bd21aaf0972e14174c0634dad335c987c4027f3b
Unknown <ale_antonioli@hotmail.com>
2018-05-23 16:24:43 +0100

    Update class MyTable

--- src/beans/MyTable.java
+++ src/beans/MyTable.java
@@ -7,28 +7,49 @@ public class MyTable {
 
 	private int id;
 	private List<MyPlayer> players = new ArrayList<MyPlayer>(4);
-	
+	private MyDeck deck = new MyDeck();
+
 	private final static int MAX_CAPACITY = 4;
-	
-	public MyTable(int id){
+
+	public MyTable(int id) {
 		this.id = id;
 	}
-	
-	public boolean addPlayer(MyPlayer playerToAdd){
-		
-		if(players.size() == MAX_CAPACITY)
+
+	public boolean addPlayer(MyPlayer playerToAdd) {
+
+		if (players.size() == MAX_CAPACITY)
 			return false;
-		
+
 		players.add(playerToAdd);
-		return true;	
+		return true;
 	}
-	
-	public void startRound(){
-		//.....
+
+	public void startRound() {
+		this.deck.shuffle();
+		// .....
 	}
-	
-	public List<MyPlayer> getPlayers(){
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public MyDeck getDeck() {
+		return deck;
+	}
+
+	public void setDeck(MyDeck deck) {
+		this.deck = deck;
+	}
+
+	public void setPlayers(List<MyPlayer> players) {
+		this.players = players;
+	}
+
+	public List<MyPlayer> getPlayers() {
 		return players;
 	}
 }
-
--- src/beans/TestBeans.java
+++ src/beans/TestBeans.java
@@ -8,6 +8,7 @@ public class TestBeans {
 		int i;
 		System.out.println("Cards:" + deck.numberOfCards());
 		for (i = 0; i < deck.numberOfCards(); i++) {
+			//deck.shuffle();
 			System.out.println("Card n:" + deck.getDeck().get(i).getValue() + " - suit: "+ deck.getDeck().get(i).getSuit());
 		}
 
